1fa08728bf7a987b1a7ff1a7bcfd444c1ebcd18b
Joan <joanetap@gmail.com>
2021-12-02 17:00:54 +0100

    Enhancement style

--- components/CheckoutCoupon.vue
+++ components/CheckoutCoupon.vue
@@ -1,13 +1,16 @@
 <script lang="ts" setup></script>
 
 <template>
-  <section class="flex border mt-2">
+  <section class="flex">
     <label for="coupon">¿Algún cupón descuento?</label>
     <input type="text" name="coupon" />
   </section>
 </template>
 
 <style scoped>
+section {
+  margin-top: 2rem;
+}
 label {
   text-align: left;
 }
